f0f27254676fe9b8d44c8f24b22fa8cba5a974d4
aimeirak <aimeirak10@gmail.com>
2021-12-21 15:22:54 +0200

    initail commit

---
+++ src/main/java/net/springcrud/ProductRepository.java
@@ -0,0 +1,7 @@
+package net.springcrud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>{
+
+}
---
+++ src/test/java/net/springcrud/productRest/ProductRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springcrud.productRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
